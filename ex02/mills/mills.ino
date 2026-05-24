a9393fbb33db5e856b1528b5bbafdec5d6df0580
ffjjdjfjfdjf-afk <ffjjdjfjfdjf@gmail.com>
2026-05-24 18:21:11 +0800

    1

--- ex02/mills/mills.ino
+++ ex02/mills/mills.ino
@@ -1,19 +1,55 @@
-const int ledPin = 2;
-unsigned long previousMillis = 0;
-const long interval = 500;  // 500ms切换一次，实现1Hz（1秒周期）
-bool ledState = LOW;
+#include <WiFi.h>
+#include <WebServer.h>
+
+const char* ap_ssid = "ESP32-LAB";
+const char* ap_pass = "12345678";  // 至少8位
+const int LED_PIN = 2;
+
+WebServer server(80);
+
+void handleRoot() {
+  String state = digitalRead(LED_PIN) ? "ON" : "OFF";
+  String html = "<html><body style='text-align:center'>"
+                "<h1>LED控制</h1><p>状态: <b>" + state + "</b></p>"
+                "<a href='/on'><button>点亮</button></a> "
+                "<a href='/off'><button>熄灭</button></a>"
+                "</body></html>";
+  server.send(200, "text/html", html);
+}
+
+void handleOn() {
+  digitalWrite(LED_PIN, HIGH);
+  server.sendHeader("Location", "/");
+  server.send(303);
+}
+
+void handleOff() {
+  digitalWrite(LED_PIN, LOW);
+  server.sendHeader("Location", "/");
+  server.send(303);
+}
 
 void setup() {
   Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
+  // AP模式：ESP32自己开热点
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ap_ssid, ap_pass);
+
+  Serial.println("热点已开启");
+  Serial.print("连接名称: ");
+  Serial.println(ap_ssid);
+  Serial.print("访问地址: http://");
+  Serial.println(WiFi.softAPIP());  // 通常是 192.168.4.1
+
+  server.on("/", handleRoot);
+  server.on("/on", handleOn);
+  server.on("/off", handleOff);
+  server.begin();
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-  
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    ledState = !ledState;
-    digitalWrite(ledPin, ledState);
-  }
+  server.handleClient();
 }
